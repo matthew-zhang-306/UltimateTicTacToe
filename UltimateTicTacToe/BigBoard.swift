11812ab4740a28644c4fe8ab1cb04759bd6239d5
Le, Brandon M <150116@s020rmc148-10.local>
2019-04-23 11:30:30 -0500

    Big Board more like big bird

--- UltimateTicTacToe/BigBoard.swift
+++ UltimateTicTacToe/BigBoard.swift
@@ -1,43 +1,53 @@
 //
-//  Board.swift
+//  BigBoard.swift
 //  UltimateTicTacToe
 //
-//  Created by Zhang, Matthew on 4/17/19.
+//  Created by Le, Brandon M on 4/23/19.
 //  Copyright © 2019 friscoisd. All rights reserved.
 //
 
 import SpriteKit
 import Foundation
 
-class Board
+class BigBoard
 {
-    var grid: [[String]]
+    var grid: [[Board]]
     var winner: String
     
     var isFull: Bool
-    var numTimesPlayed: Int = 0
+    var numBoardsFull: Int = 0
+    
+    var selectedBoard: CGPoint
     
     init()
     {
-        grid = [[" "," "," "],[" "," "," "],[" "," "," "]]
+        grid = [[Board(),Board(),Board()],[Board(),Board(),Board()],[Board(),Board(),Board()]]
         winner = " "
+        
         isFull = false
+        
+        selectedBoard = CGPoint(x: 0, y: 0)
     }
     
-    func play(_ player: String, at pos: CGPoint)
-    {
-        let x = Int(pos.x)
-        let y = Int(pos.y)
+    func play(_ player: String, at pos: CGPoint) {
+        var x = Int(pos.x)
+        var y = Int(pos.y)
         
-        guard x >= 0 && x < 3 && y >= 0 && y < 3 else { return }
-        guard grid[y][x] == " " else { return }
+        guard x >= 0 && x < 9 && y >= 0 && y < 9 else { return; }
         
-        grid[y][x] = player
+        let boardX = x % 3
+        let boardY = y % 3
+        x = x / 3
+        y = y / 3
+        
+        guard grid[y][x].grid[boardY][boardX] == " " else { return }
+        
+        grid[y][x].play(player, at: CGPoint(x: boardX, y: boardY))
         
         // check for winner
         var col = 0;
         while (col < 3) {
-            if (grid[col][x] != player) {
+            if (grid[col][x].winner != player) {
                 break
             }
             col += 1
@@ -49,7 +59,7 @@ class Board
         
         var row = 0;
         while (row < 3) {
-            if (grid[y][row] != player) {
+            if (grid[y][row].winner != player) {
                 break
             }
             row += 1
@@ -62,7 +72,7 @@ class Board
         if (y == x) {
             row = 0;
             while (row < 3) {
-                if (grid[row][row] != player) {
+                if (grid[row][row].winner != player) {
                     break
                 }
                 row += 1
@@ -76,7 +86,7 @@ class Board
         if (y == 2 - x) {
             row = 0;
             while (row < 3) {
-                if (grid[row][2 - row] != player) {
+                if (grid[row][2 - row].winner != player) {
                     break
                 }
                 row += 1
@@ -88,11 +98,21 @@ class Board
         }
         
         
-        numTimesPlayed += 1
-        if (numTimesPlayed == 9) {
-            isFull = true
+        if (grid[y][x].isFull) {
+            numBoardsFull += 1
+            if (numBoardsFull == 9) {
+                isFull = true
+            }
         }
         
+        
+        // set selected board
+        if (grid[boardY][boardX].isFull) {
+            selectedBoard = CGPoint(x: -1, y: -1)
+        }
+        else {
+            selectedBoard = CGPoint(x: boardX, y: boardY)
+        }
         
     }
     
