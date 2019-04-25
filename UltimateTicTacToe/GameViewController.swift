af6adb5c026980399b7154ca0e5c997387c414c7
Le, Brandon M <150116@s020rmc148-10.local>
2019-04-25 12:09:59 -0500

    Buttons in a grid doing things that work sometimes possibly maybe

--- UltimateTicTacToe/GameViewController.swift
+++ UltimateTicTacToe/GameViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
+class GameViewController: UIViewController
+{
+    var board = Board()
+    
+    var buttons: [[UIButton]]!
+
+    
+    var currentPlayer = "X"
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
+        if let view = self.view as! SKView?
+        {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = SKScene(fileNamed: "GameScene")
+            {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
@@ -30,26 +41,145 @@ class GameViewController: UIViewController {
             view.showsFPS = true
             view.showsNodeCount = true
         }
+        
+        buttons = makeButtonArray()
+        smallBoardDraw(board: board)
+        
+        print(buttons)
     }
 
-    override var shouldAutorotate: Bool {
+    func bigBoardDraw()
+    {
+        
+    }
+    
+    func smallBoardDraw(board: Board)
+    {
+        //For each button in buttons or each button in grid, add the buttons to the subview
+        for buttonArray in buttons
+        {
+            for button in buttonArray
+            {
+                self.view.addSubview(button)
+                
+            }
+
+        }
+        
+        //MOVED TO MAKE BUTTON ARRAY
+//        var buttonLength = 50
+//        var col = 0
+//        var row = 0
+//        var x = 0
+//        var y = 0
+//        while(row < board.grid.count)
+//        {
+//            while(col < board.grid[row].count)
+//            {
+//                let button = UIButton(frame: CGRect(x: x, y: y, width: buttonLength, height: buttonLength))
+//                button.backgroundColor = .green
+//                button.setTitle(board.grid[row][col], for: .normal)
+//                button.setTitleColor(.black, for: .normal)
+//                button.addTarget(self, action: #selector(buttonTest), for: .touchUpInside)
+//                self.view.addSubview(button)
+//
+//                x += buttonLength + 1
+//                col += 1
+//            }
+//            x = 0
+//            y += buttonLength + 1
+//            col = 0
+//            row += 1
+//        }
+    }
+    
+    func makeButtonArray() -> [[UIButton]]
+    {
+        var buttons = [[UIButton]]()
+        let buttonLength = 50
+        var col = 0
+        var row = 0
+        var x = 0
+        var y = 0
+        while(row < board.grid.count)
+        {
+            var buttonRow = [UIButton]()
+            while(col < board.grid[row].count)
+            {
+                let button = UIButton(frame: CGRect(x: x, y: y, width: buttonLength, height: buttonLength))
+                button.backgroundColor = .green
+                button.setTitle(board.grid[row][col], for: .normal)
+                button.setTitleColor(.black, for: .normal)
+                button.addTarget(self, action: #selector(buttonTest), for: .touchUpInside)
+                buttonRow.append(button)
+    
+                x += buttonLength + 1
+                col += 1
+            }
+            buttons.append(buttonRow)
+            x = 0
+            y += buttonLength + 1
+            col = 0
+            row += 1
+        }
+        return buttons
+    }
+    
+    @IBAction func buttonTest(_ sender: UIButton)
+    {
+        sender.backgroundColor = .red
+        sender.setTitle(currentPlayer, for: .normal)
+        
+        if(currentPlayer == "X")
+        {
+            currentPlayer = "O"
+        }
+        else
+        {
+            currentPlayer = "X"
+        }
+        
+        var pos: CGPoint = findButton(sender)
+        print("button tapped \(pos)")
+    }
+    
+    func findButton(_ sender: UIButton) -> CGPoint {
+        for var y in 0...2 {
+            for var x in 0...2 {
+                if (buttons[y][x] == sender) {
+                    return CGPoint(x: x, y: y)
+                }
+            }
+        }
+        return CGPoint(x: -1, y: -1)
+    }
+    
+    override var shouldAutorotate: Bool
+    {
         return true
     }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask
+    {
+        if UIDevice.current.userInterfaceIdiom == .phone
+        {
             return .allButUpsideDown
-        } else {
+        }
+        else
+        {
             return .all
         }
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
 
-    override var prefersStatusBarHidden: Bool {
+    override var prefersStatusBarHidden: Bool
+    {
         return true
     }
+
 }
